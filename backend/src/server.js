019a6221e0c8abaade652e2428c579c1e18acded
Carlos Ochoa <emilianoochoa088@gmail.com>
2026-09-17 10:29:00 -0600

    2nd change to backend for railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`LOTO API escuchando en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`LOTO API escuchando en puerto ${PORT}`);
 });
