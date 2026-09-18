c652e4d32cdc56be141fe352a67fb5f2028ccd2c
Carlos Ochoa <emilianoochoa088@gmail.com>
2026-09-18 10:46:46 -0600

    Ncessary changes 2

--- LOTOdigital-2main/LOTOdigital-main/backend/src/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma;
-}
-
-module.exports = prisma;
